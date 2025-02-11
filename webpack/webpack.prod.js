a1c7bb4dbcb04cb235600d69a3118bacf35ab7b1
Jayaramachandran Augustin <david.david188@gmail.com>
2025-02-11 00:21:36 -0600

    configuration to use different webpack config for prod and dev

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,11 @@
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    devtool: "source-map",
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+};
